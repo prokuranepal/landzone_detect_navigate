3c634ac3ba6fb0a0e46dc2b3488a43b66b107799
shovan777 <raj.shrestha778@gmail.com>
2019-05-08 17:34:05 +0545

    Add socket client. Add read json.

--- event_driven/socket_python_2_client.py
+++ event_driven/socket_python_2_client.py
@@ -3,20 +3,38 @@ import json
 
 sio = socketio.Client()
 
-@sio.on('connect',namespace='/')
+
+@sio.on('connect', namespace='/')
 def on_connect():
     print('connection established')
+    sio.emit('message', {"dfa": "dfsa"})
 
-@sio.on('reply',namespace='/')
+
+@sio.on('reply', namespace='/')
 def on_message(data):
     print(json.dumps(data))
-    sio.emit('message', {'chor':'shovan daka'})
+    #sio.emit('message', {'chor':'shovan daka'})
 
-@sio.on('disconnect',namespace='/')
+
+@sio.on('angle', namespace='/')
+def on_angle(data):
+    print(json.dumps(data))
+    with open('test.txt', 'w') as f:
+        json.dump(data, f)
+    sio.emit('message', {'chor': 'shovan daka'})
+
+# @sio.on('message1',namespace='/')
+# def on_message1(data):
+    # print(data)
+
+
+@sio.on('disconnect', namespace='/')
 def on_disconnect():
     print('disconnected from server')
 
-#sio.connect('http://192.168.1.69:8080')
-sio.connect('http://localhost:8080')
-sio.emit('message',{'chor':'shovan is daka'})
+
+sio.connect('http://10.42.0.1:8080')
+# sio.connect('http://192.168.43.135:8080')
+# sio.emit('message1',{'dfa':"sdf"})
+# sio.connect('http://localhost:8080')
 sio.wait()
